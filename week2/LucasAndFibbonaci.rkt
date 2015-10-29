ee386caca64eb5303f2517ef78f115ff74b54c20
Banned365 <assotirov1@abv.bg>
2015-10-29 21:42:01 +0000

    Create LucasAndFibbonaci.rkt

---
+++ week2/LucasAndFibbonaci.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (fib n)
+  (define (fib-iter a b current)
+    (if (= n current) b
+        (fib-iter b (+ a b) (add1 current)))
+     )
+    (fib-iter 1 1 2))
+
+(define (lucas n)
+  (define (lucas-iter a b current)
+    (if (= n current) b
+        (lucas-iter b (+ a b) (add1 current)))
+     )
+    (lucas-iter 2 1 2))
+
